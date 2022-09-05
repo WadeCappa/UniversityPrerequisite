3256c8151f2fce9658556ddf199ae4036251cffb
Cappa <wade.cappa@msn.com>
2022-09-04 18:41:09 -0700

    finished overengineering the 'getData' request, realized that it has no reason to be this complicated, literally just return a map of IDs to task information + a list of all tasks that could be in path

--- api_v2/src/main/scala/data/DBManager.scala
+++ api_v2/src/main/scala/data/DBManager.scala
@@ -10,6 +10,8 @@ import io.finch._
 import doobie.implicits._
 import models._
 
+import scala.collection.mutable
+
 case class DBManager(db: Aux[IO, Unit]) {
 
   def getOrgs(): IO[Output[List[Org]]] = {
@@ -29,19 +31,40 @@ case class DBManager(db: Aux[IO, Unit]) {
 
   // TODO: Builds a tree. Not nearly as efficent as it should be. The database should do this for you instead of this
   //  terrible work-a-round. Eventually move to Neo4j. For now this works as a POC
+
+  // TODO: This function is terribly inefficent. You need to rework this, Neo4j might be your solution.
   private def getAllCoursesInPath(org: Org, objectives: List[Objective]): ExecutableTree = {
     val courses: List[Task] = rawData[Task](sql"select * from task where parent_org = ${org.organization_id}")
 
+    val courseInfo: Map[Int, Task] = courses.map { task =>
+      (task.task_id, task)
+    }.toMap
+
+    // parent_task, path_id, child_task
     val paths = rawData[(Int, Int, Int)](
       sql"select parent_id, path_id, child_executable_id from path, task, in_path where path.parent_id = task.task_id and task.parent_org = ${org.organization_id} and in_path.parent_path = path.path_id order by parent_id, path_id"
     )
 
+    // Map[Path_id, List[Task_id]]
+    val listOfPaths: Map[Int, List[Int]] = paths.foldLeft(Map[Int, List[Int]]())((ac, t) => {
+      ac.exists(_ == t._2) match {
+        case false => ac + (t._2 -> List(t._3))
+        case true  => ac + (t._2 -> (t._3 :: ac(t._2)))
+      }
+    })
+
+    val taskMap: Map[Int, List[List[Int]]] = paths.foldLeft(Map[Int, List[List[Int]]]())((ac, t) => {
+      ac.exists(_ == t._1) match {
+        case false => ac + (t._1 -> List(listOfPaths(t._2)))
+        case true  => ac + (t._1 -> (listOfPaths(t._2) :: ac(t._1)))
+      }
+    })
+
     val head: ExecutableTree =
       ExecutableTree(Objective(0, org.organization_id, Some("Custom objective"), None), List[ExecutableTree]())
 
-    val res = (h: ExecutableTree, c: List[Task], p: List[(Int, Int, Int)]) => {}
+    val res = (parent: ExecutableTree) => {}
 
-    res(head, courses, paths)
     head
   }
 
