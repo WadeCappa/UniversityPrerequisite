687f1298c505f9e4550b23a1814c56a1354aa02d
Cappa <wade.cappa@msn.com>
2022-09-24 15:30:15 -0700

    added dragability, can visually create schedules, now need to add fuctionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,57 @@ import { useEffect, useState } from 'react';
 
 const outputData = (data) => {
   return (
-    Object.keys(data).map(key => {
+    Object.keys(data).reverse().map(key => {
       let d = data[key]
-      return (<li key={d.data.id}>{d.data.subject}: {d.data.number} {"->"} {d.data.title}</li>)
+      return (<Task task={d}/>)
     })
   )
 }
+
+function Task({task}) {
+  return (
+    <div className="task" draggable="true" onDragStart={onDragStart} key={task.data.id} id={`task: ${task.data.subject} ${task.data.number}`}>
+      {task.data.subject} {task.data.number} {"->"} {task.data.title}
+    </div>
+  )
+}
+
+function Semester({size, index}) {
+  return (
+    <div className="semester" key={`semester: ${index + 1}`}>
+      <h2>Semester {index}</h2>
+      <div onDragOver={onDragOver} onDrop={onDrop}>
+        courses:
+      </div>
+    </div>
+  )
+}
+
+
+function onDragStart(event) {
+  event
+    .dataTransfer
+    .setData('text/plain', event.target.id);
+}
+
+function onDragOver(event) {
+  event.preventDefault();
+}
+
+function onDrop(event) {
+  event.preventDefault();
+
+  event.target.appendChild(
+    document.getElementById(
+      event.dataTransfer.getData('text')
+    )
+  );
+
+  event
+    .dataTransfer
+    .clearData();
+}
+
 
 function App() {
   const [currentState, setState] = useState([]);
@@ -19,14 +64,19 @@ function App() {
     setState(response);
   }
 
-
-
   useEffect(() => {getCourses()}, []);  
 
   return (
     <div>
 
-      {outputData(currentState)}
+      <div className="row">
+        <div className="left">
+          {Array.apply(null, Array(8)).map((_,i) => {return <Semester size={6} index={i + 1}/>})}
+        </div>
+        <div className="right">
+          {outputData(currentState)}
+        </div>
+      </div> 
 
     </div>
   );
