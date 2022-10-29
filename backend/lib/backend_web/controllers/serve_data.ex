34fb51d46909e7718dbfd434686e6a0e2c547972
Cappa <wade.cappa@msn.com>
2022-10-29 16:02:09 -0700

    finished basic first route

--- backend/lib/backend_web/controllers/serve_data.ex
+++ backend/lib/backend_web/controllers/serve_data.ex
@@ -5,10 +5,10 @@ defmodule BackendWeb.ServeData do
     {:ok, _pid} = Bolt.Sips.start_link(url: "prereq://localhost", port: 7687, basic_auth: [username: "neo4j", password: "localpass"])
 
     connection = Bolt.Sips.conn()
-    res = Bolt.Sips.query!(connection, "match (o: Organization) return ID(o), o.title, o.slots_per_bucket")
+    %Bolt.Sips.Response{results: res} = Bolt.Sips.query!(connection, "match (o: Organization) return ID(o), o.title, o.slots_per_bucket")
 
     IO.inspect(res)
 
-    text conn, "backend!"
+    json conn, res
   end
 end
