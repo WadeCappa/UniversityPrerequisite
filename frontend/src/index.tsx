573ba3a4c26b6ccd678b28b30afc1aabc4da1950
Cappa <wade.cappa@msn.com>
2022-12-19 13:48:42 -0800

    fixed bug with npm install for frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Orgs from "./views/Orgs";
-import Degrees from "./views/Degrees";
-import NewSchedule from "./views/NewSchedule";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error('Failed to find the root element');
-const root = createRoot(rootElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+reportWebVitals();
