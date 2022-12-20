a347b04e859683422d57e609b5454f920c4514cf
Cappa <wade.cappa@msn.com>
2022-12-20 13:41:31 -0800

    initialized new express backend api

---
+++ backend/data_management/runCypher.js
@@ -0,0 +1,31 @@
+const neo4j = require('neo4j-driver');
+
+class DatabaseManager
+{
+    constructor() {
+        this.uri = process.env.URI;
+        this.user = 'neo4j';
+        this.password = process.env.PASSWORD;
+        
+        // To learn more about the driver: https://neo4j.com/docs/javascript-manual/current/client-applications/#js-driver-driver-object
+        this.driver = neo4j.driver(this.uri, neo4j.auth.basic(this.user, this.password));
+    }
+
+    runQuery = async(cypherText) => {
+        const session = this.driver.session({ database: 'neo4j' });
+        const result = await this.queryDatabase(cypherText, session);
+        result.records.forEach(record => {
+            console.log(`Found: ${record.get("p.subject"), record.get("p.number")}`)
+        });
+        session.close();
+    }
+
+    queryDatabase = async(cypherText, session) => {
+        return await session.executeRead(tx => tx.run(cypherText));
+    }
+
+};
+
+
+
+module.exports = DatabaseManager
